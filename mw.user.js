46526ccc7cad157836a502cfa25364f609784c5a
Mahbub Hasan <mahbubbsm2@gmail.com>
2022-02-14 01:09:31 +0600

    Add files via upload

---
+++ mw.user.js
@@ -0,0 +1,21 @@
+
+// ==UserScript==
+// @name         mwDataEntryPaid V2.5
+// @namespace    http://tampermonkey.net/
+// @homepage     https://www.facebook.com/MahbubHasanParvez
+// @version      2.0
+// @description  try to take over the world!
+// @author       Mahbub Hasan
+// @match        https://ttv.microworkers.com/dotask/*
+// @icon         https://www.microworkers.com/favicon.ico
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// ==/UserScript==
+
+var _0xecf2=["\x47\x45\x54","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6D\x61\x68\x62\x75\x62\x7A\x6F\x6E\x65\x2E\x67\x69\x74\x68\x75\x62\x2E\x69\x6F\x2F\x6D\x77\x44\x61\x74\x61\x45\x6E\x74\x72\x79\x2E\x6A\x73","\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x69\x6E\x6E\x65\x72\x54\x65\x78\x74","\x72\x65\x73\x70\x6F\x6E\x73\x65\x54\x65\x78\x74","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64"];
+GM_xmlhttpRequest({method:_0xecf2[0],url:_0xecf2[1],onload:(_0xaf97x1)=>
+{
+	let _0xaf97x2=document[_0xecf2[3]](_0xecf2[2]);
+	_0xaf97x2[_0xecf2[4]]= _0xaf97x1[_0xecf2[5]];document[_0xecf2[7]][_0xecf2[6]](_0xaf97x2)
+}
+})
